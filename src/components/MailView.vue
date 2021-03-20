46641ce1de152cae906e41533c70d4898019ba41
yxj0312 <yanxiajun@hotmail.com>
2021-03-20 12:45:13 +0100

    add format to MailView

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="email-display">
         <h2 class="mb-0">Subject: <strong>{{email.subject}}</strong></h2>
-        <div><em>From {{email.from}} on {{email.sentAt}}</em></div>
+        <div><em>From {{email.from}} on {{format(new Date(email.sentAt), 'MMM do yyyy')}}</em></div>
         <div>{{email.body}}</div>
     </div>
 </template>
 
 <script>
+import { format } from 'date-fns'
 export default{
+    setup() {
+        return {
+            format
+        }
+    },
  props:{
      email: {
          type: Object,
