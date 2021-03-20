c1d1e14c3039c0c70e31b7cc2e0f66ad5b82568e
yxj0312 <yanxiajun@hotmail.com>
2021-03-20 12:46:50 +0100

    Add markdown

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -2,16 +2,18 @@
     <div class="email-display">
         <h2 class="mb-0">Subject: <strong>{{email.subject}}</strong></h2>
         <div><em>From {{email.from}} on {{format(new Date(email.sentAt), 'MMM do yyyy')}}</em></div>
-        <div>{{email.body}}</div>
+        <div v-html="marked(email.body)"></div>
     </div>
 </template>
 
 <script>
 import { format } from 'date-fns'
+import marked from 'marked'
 export default{
     setup() {
         return {
-            format
+            format,
+            marked
         }
     },
  props:{
