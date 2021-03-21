bc72e53989814d23f9859fa6a234bfd3fb032640
yxj0312 <yanxiajun@hotmail.com>
2021-03-21 22:30:00 +0100

    register keydown as object

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -9,10 +9,12 @@
 
 <script>
   import { onBeforeUnmount } from 'vue';
-  let useKeydown = (keyPressed, fn) => {
+  // let useKeydown = (keyPressed, fn) => {
+  let useKeydown = (keyCombo) => {
      let onKeydown = (event) => {
-        if (event.key == keyPressed) {
-          fn()
+        // if (event.key == keyPressed) {
+        if (event.key == keyCombo.key) {
+          keyCombo.fn()
         }
       }
       window.addEventListener('keydown', onKeydown);
@@ -27,8 +29,11 @@
     // and notice that it’s logging twice per key press. Then close it down, and notice it’s still logging twice per key press. 
     // That’s because we’re not removing the event listeners, so they stick around after closing the modal, and stack up every time we open the modal again.
     setup(props, {emit}) {
-      useKeydown('Escape', () => {emit('closeModal')})
-      useKeydown('Enter', () => {console.log('A different function')})
+      // useKeydown('Escape', () => {emit('closeModal')})
+      useKeydown({key:'Escape', fn:() => {emit('closeModal')}})
+      // issue: it shows enter twice, because onKeydown is registered twice
+      // useKeydown('Enter', () => {console.log('A different function')})
+      useKeydown({key:'Enter', fn:() => {console.log('A different function')}})
 
       return { 
 	      emit
