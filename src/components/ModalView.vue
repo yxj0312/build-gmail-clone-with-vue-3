711e8c2714fd5c6a0e758e36c526d06700326183
yxj0312 <yanxiajun@hotmail.com>
2021-03-21 22:24:42 +0100

    Extract keydown function

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -9,15 +9,10 @@
 
 <script>
   import { onBeforeUnmount } from 'vue';
-  export default {
-    // Close it down, and open it up again… 
-    // and notice that it’s logging twice per key press. Then close it down, and notice it’s still logging twice per key press. 
-    // That’s because we’re not removing the event listeners, so they stick around after closing the modal, and stack up every time we open the modal again.
-    setup(props, {emit}) {
-      let onKeydown = (event) => {
-        console.log(event.key)
-        if (event.key == 'Escape') {
-          emit('closeModal')
+  let useKeydown = (keyPressed, fn) => {
+     let onKeydown = (event) => {
+        if (event.key == keyPressed) {
+          fn()
         }
       }
       window.addEventListener('keydown', onKeydown);
@@ -26,6 +21,15 @@
       onBeforeUnmount(() => {
         window.removeEventListener('keydown', onKeydown)
       })
+  }
+  export default {
+    // Close it down, and open it up again… 
+    // and notice that it’s logging twice per key press. Then close it down, and notice it’s still logging twice per key press. 
+    // That’s because we’re not removing the event listeners, so they stick around after closing the modal, and stack up every time we open the modal again.
+    setup(props, {emit}) {
+      useKeydown('Escape', () => {emit('closeModal')})
+      useKeydown('Enter', () => {console.log('A different function')})
+
       return { 
 	      emit
       }
