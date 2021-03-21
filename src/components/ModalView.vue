44d78285e1cacab6069df7b1268072df09b9607b
yxj0312 <yanxiajun@hotmail.com>
2021-03-21 21:11:17 +0100

    Add a keydown for closemodal

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -9,9 +9,14 @@
 
 <script>
   export default {
+    // Close it down, and open it up again… 
+    // and notice that it’s logging twice per key press. Then close it down, and notice it’s still logging twice per key press. 
+    // That’s because we’re not removing the event listeners, so they stick around after closing the modal, and stack up every time we open the modal again.
     setup(props, {emit}) {
       let onKeydown = (event) => {
-        console.log(event.key);
+        if (event.key == 'Escape') {
+          emit('closeModal')
+        }
       }
       window.addEventListener('keydown', onKeydown)
       return { 
