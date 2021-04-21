1f1088015e175bb86e29f8f3a7560ae81d07e05c
yxj0312 <yanxiajun@hotmail.com>
2021-04-21 22:27:46 +0200

    moves reactive emails out, to fix the bug

--- src/composables/use-email-selection.js
+++ src/composables/use-email-selection.js
@@ -1,10 +1,10 @@
 import { reactive } from 'vue';
 
+let emails = reactive(new Set())
 export const useEmailSelection = function () {
     // emailSeIected and removeSelection() would each take multiple lines, or at least one longer line, to implement if we used in Array… but if we use a Set, then they both come built-in
     // has , delete, and add do exactly what you’d expect.
     // A value in the Set may only occur once, it is unique in the Set's collection
-    let emails = reactive(new Set())
     let toggle = function(email) {
             if (emails.has(email)) {
                 emails.delete(email)
