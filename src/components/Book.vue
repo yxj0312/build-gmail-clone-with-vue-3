a370b4c3d33710067c6db97fe870a632a211e4c5
yxj0312 <yanxiajun@hotmail.com>
2022-08-02 21:34:20 +0200

    add two components

---
+++ src/components/Book.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="book">
+    <slot name="title"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bookTitle: 'Child Providing Data'
+    }
+  }
+}
+</script>
---
+++ src/components/Library.vue
@@ -0,0 +1,7 @@
+<template>
+  <Book>
+    <template v-slot:title>
+      <!-- How do we get the bookTitle from Book.vue? -->
+    </template>
+  </Book>
+</template>
