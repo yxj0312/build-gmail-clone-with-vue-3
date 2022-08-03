77a2a7508df51ede4cf83c9515026c3ea04b54d5
yxj0312 <yanxiajun@hotmail.com>
2022-08-03 22:20:39 +0200

    refactor

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -1,8 +1,10 @@
 <template>
   <Book>
-    <template v-slot:title="slotProps">
+    <!-- <template v-slot:title="slotProps"> -->
+    <template v-slot:title="{ bookTitle }">
       <!-- How do we get the bookTitle from Book.vue? -->
-      <h1>{{ slotProps.bookTitle }}</h1>
+      <!-- <h1>{{ slotProps.bookTitle }}</h1> -->
+      <h1>{{ bookTitle }}</h1>
     </template>
   </Book>
 </template>
