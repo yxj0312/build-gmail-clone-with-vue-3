bbbd0703a5c03e63f5a0f35a521ba65ee2ed2655
yxj0312 <yanxiajun@hotmail.com>
2022-08-05 22:27:48 +0200

    add a new component

---
+++ src/components/VueMasteryLogo.vue
@@ -0,0 +1,16 @@
+<template>
+    <img v-bind:src="imageAttrs.src" v-bind:alt="imageAttrs.alt">
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            imageAttrs: {
+                src: '/vue-mastery-logo.png'
+                alt: 'Vue Mastery Logo'
+            }
+        }
+    }
+}
+</script>
