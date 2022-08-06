5fdaf3a2bb0dc1ef072ea4aef847706445395f15
yxj0312 <yanxiajun@hotmail.com>
2022-08-06 22:27:06 +0200

    add complex example

---
+++ src/components/NewsFeed.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <Component 
+      v-for="content in apiResponse"
+      :key="content.id"
+      :is="content.type"
+      :article-title="content.title"
+      :article-content="content.body"
+      :ad-image="content.image"
+      :ad-heading="content.heading"
+      @click="content.type === 'NewsArticle' ? openArticle : openAd"
+      @mouseover="content.type === 'NewsArticle' ? showPreview : trackAdEvent"
+    />
+  </main>
+</template>
