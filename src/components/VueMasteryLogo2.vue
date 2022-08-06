d71c1a200c71aa290221400893ee2bbbd2c68cb7
yxj0312 <yanxiajun@hotmail.com>
2022-08-06 22:13:18 +0200

    refactor

--- src/components/VueMasteryLogo2.vue
+++ src/components/VueMasteryLogo2.vue
@@ -1,13 +1,20 @@
 <template>
     <!-- <img v-on:click="openGallery" v-on:mouseover="showTooltip" /> -->
-     <img v-on="{ click: openGallery, mouseover: showTooltip }" />
+     <!-- <img v-on="{ click: openGallery, mouseover: showTooltip }" /> -->
+      <img v-on="inputEvents" />
 </template>
 
 <script>
 export default {
-  methods: {
-    openGallery() {  },
-    showTooltip() {  }
-  }
+    computed: {
+        inputEvents: {
+        click: this.openGallery,
+        mouseover: this.showTooltip
+        }
+    },
+    methods: {
+        openGallery() {  },
+        showTooltip() {  }
+    }
 }
 </script>
