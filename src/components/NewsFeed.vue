b56a5bbee1997faec439b5aa38c5c556619147ce
yxj0312 <yanxiajun@hotmail.com>
2022-08-06 22:28:25 +0200

    add refactor

--- src/components/NewsFeed.vue
+++ src/components/NewsFeed.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <main>
     <Component 
       v-for="content in apiResponse"
@@ -12,4 +12,48 @@
       @mouseover="content.type === 'NewsArticle' ? showPreview : trackAdEvent"
     />
   </main>
+</template> -->
+
+<template>
+  <main>
+    <Component 
+      v-for="content in apiResponse"
+      :key="content.id"
+      :is="content.type"
+      v-bind="feedItem(content).attrs"
+      v-on="feedItem(content).events"
+    />
+  </main>
 </template>
+
+<script>
+export default {
+  methods: {
+    feedItem(item) {
+      if (item.type === 'NewsArticle') {
+        return {
+          attrs: {
+            'article-title': item.title,
+            'article-content': item.content
+          },
+          events: {
+            click: this.openArticle,
+            mouseover: this.showPreview
+          }
+        }
+      } else if (item.type === 'NewsAd') {
+        return {
+          attrs: {
+            'ad-image': item.image,
+            'ad-heading': item.heading
+          },
+          events: {
+            click: this.openAd,
+            mouseover: this.trackAdEvent
+          }
+        }
+      }
+    }
+  }
+}
+</script>
