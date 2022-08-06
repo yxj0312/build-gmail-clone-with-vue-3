52169a4cf985f8694a8168afa6df41d9e150401b
yxj0312 <yanxiajun@hotmail.com>
2022-08-06 22:11:40 +0200

    add v-on example

---
+++ src/components/VueMasteryLogo2.vue
@@ -0,0 +1,12 @@
+<template>
+    <img v-on:click="openGallery" v-on:mouseover="showTooltip" />
+</template>
+
+<script>
+export default {
+  methods: {
+    openGallery() {  },
+    showTooltip() {  }
+  }
+}
+</script>
